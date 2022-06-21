b91b917ef1a6db0d62703a8a569977d0b84bf4c7
Julius Nartey <julius.nartey.71@gmail.com>
2022-06-21 06:11:47 +0000

    added CORS to main.py

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from config import settings
+from .routers import post
+from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+origins = ["*"]
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(post.router)
+
+
