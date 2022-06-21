4dc8004cebf8fae3e98984d278e417691f94c391
Julius Nartey <julius.nartey.71@gmail.com>
2022-06-21 06:12:35 +0000

    models modified

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import relationship
 
 from blog.database import Base
 
-
 class Post(Base):
 
     __tablename__ = "posts"
@@ -16,4 +15,4 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
-    # owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
